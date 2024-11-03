b2404a0e3db4ab04c7d35ce3556e3378cd1ddd67
soberanalysts <nss262@naver.com>
2024-11-03 23:10:16 +0900

    이미지 슬라이더

---
+++ homework/imageSlider1/img.js
@@ -0,0 +1,18 @@
+var photo = document.getElementById("photo");
+var thumbnail = document.querySelectorAll("#gallery > li > img");
+
+for (var i = 0; i < thumbnail.length; i++) {
+    thumbnail[i].addEventListener("click", function () {
+        photo.setAttribute("src", this.getAttribute("src"));
+    });
+}
+
+var photo = document.getElementById("photo");
+var thumbnail = document.querySelectorAll("#gallery > li > img");
+
+for ( var i = 0; i < thumbnail.length; i++ )
+  thumbnail[i].addEventListener("click", function () {
+
+    photo.setAttribute("src", this.getAttribute("src"));
+
+  });  
